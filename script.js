b48585f7d0cabf9357e7b10ec5673081245f1858
reyna458 <145049019+reyna458@users.noreply.github.com>
2025-09-04 11:24:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    const fonts = [
+        `"Geologica", sans-serif`,
+        `"UnifrakturMaguntia", cursive`,
+        `"Festive", cursive`,
+        `"Sue Ellen Francisco", cursive`,
+        `"Oxanium", sans-serif`,
+        `"Kablammo", system-ui`,
+        `"Newsreader", serif`,
+        `"Libertinus Keyboard", system-ui`,
+        `"Rubik Glitch", system-ui`,
+        `"Bungee Shade", sans-serif`,
+        `"Caesar Dressing", system-ui`,
+        `"Press Start 2P", system-ui`,
+        `"Fruktur", system-ui;`,
+        `"Nabla", system-ui;`,
+        `"Plaster", system-ui`,
+        `"Nosifer", sans-serif`,
+        `"Rubik Puddles", system-ui`,
+        `"Danfo", serif;`,
+        `"Princess Sofia", cursive;`,
+        `"Monoton", sans-serif`
+    ];
+
+    $('span').click(function() {
+
+        $('.chosen').removeClass('chosen')
+        $(this).addClass('chosen')
+
+        // loop through all spans except the one clicked
+        $('span').not(this).each(function () {
+            let randomFont = fonts[Math.floor(Math.random() * fonts.length)];
+            $(this).css("font-family", randomFont);
+        });
+    });
+});
